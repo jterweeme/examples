ec2b4fda23b792f5c20709a6b5ea607775ba9560
Jasper ter Weeme <jterweeme@gmail.com>
2021-03-15 01:33:05 +0100

    globals verwijderd uit pcat

--- pcat/main.cpp
+++ pcat/main.cpp
@@ -1,35 +1,21 @@
-#include <stdio.h>
-#include <fcntl.h>
 #include <unistd.h>
 #include <stdint.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 #include <stdexcept>
 #include <iostream>
 #include <iostream>
 
-static char *outp;
-static char outbuff[BUFSIZ];
-static short outfile;
-static char *inp;
-static char inbuff[BUFSIZ];
-static short inleft, infile;
-static long origsize;
-static char *xeof;
-static short intnodes[25];
-static char *tree[25];
-
-static int decode()
+static int decode(const char *xeof, const short intnodes[25], char **tree, long origsize, short infile, short inleft, char *inbuff, char *inp)
 {
-    int bitsleft, c;
-    int j;
-    char *p;
-
-    outp = &outbuff[0];
-    int lev = 1;
+    char g_outbuff[BUFSIZ];
+    char *g_outp = &g_outbuff[0];
+    uint32_t lev = 1;
     int i = 0;
+#if 0
+    std::cerr << inleft << "\r\n";
+    std::cerr.flush();
+#endif
 
-    while (1)
+    while (true)
     {
         if (inleft <= 0)
         {
@@ -41,15 +27,14 @@ static int decode()
 
         if (--inleft < 0)
         {
-uggh:
             if (origsize == 0)
                 return 1;
 
             throw std::runtime_error(".z: unpacking error");
         }
 
-        c = *inp++;
-        bitsleft = 8;
+        int c = *inp++;
+        int bitsleft = 8;
 
         while (--bitsleft >= 0)
         {
@@ -59,139 +44,120 @@ uggh:
                 i++;
 
             c <<= 1;
+            int j = i - intnodes[lev];
 
-            if ((j = i - intnodes[lev]) >= 0)
+            if (j < 0)
             {
-                p = &tree[lev][j];
-
-                if (p == xeof)
-                {
-                    c = outp - &outbuff[0];
-
-                    if (write (outfile, &outbuff[0], c) != c)
-                    {
-wrerr:
-                        throw std::runtime_error(": write error");
-                    }
-                    origsize -= c;
-
-                    if (origsize != 0)
-                        goto uggh;
-
-                    return (1);
-                }
-                *outp++ = *p;
-
-                if (outp == &outbuff[BUFSIZ])
-                {
-                    if (write(outfile, outp = &outbuff[0], BUFSIZ) != BUFSIZ)
-                        goto wrerr;
-
-                    origsize -= BUFSIZ;
-                }
-
-                lev = 1;
-                i = 0;
+                ++lev;
+                continue;
             }
-            else
+
+            const char *p = &tree[lev][j];
+
+            if (p == xeof)
             {
-                lev++;
+                c = g_outp - &g_outbuff[0];
+
+                if (write(1, &g_outbuff[0], c) != c)
+                    throw std::runtime_error(": write error");
+
+                origsize -= c;
+
+                if (origsize != 0)
+                    throw std::runtime_error(".z: unpacking error");
+
+                return (1);
             }
+            *g_outp++ = *p;
+
+            if (g_outp == &g_outbuff[BUFSIZ])
+            {
+                if (write(1, g_outp = &g_outbuff[0], BUFSIZ) != BUFSIZ)
+                    throw std::runtime_error(": write error");
+
+                origsize -= BUFSIZ;
+            }
+
+            lev = 1;
+            i = 0;
         }
     }
 }
 
-static short maxlev;
-static char characters[256];
-
-int getdict()
+static int getdict(short infile)
 {
-    int c, i, nchildren;
-    xeof = &characters[0];
-    inbuff[6] = 25;
-    inleft = read(infile, &inbuff[0], BUFSIZ);
+    char g_inbuff[BUFSIZ];
+    g_inbuff[6] = 25;
+    short g_inleft = read(infile, &g_inbuff[0], BUFSIZ);
 
-    if (inleft < 0)
+    if (g_inleft < 0)
         throw std::runtime_error(".z: read error");
 
-    if (inbuff[0] != 0x1f)
+    if (g_inbuff[0] != 0x1f)
         throw std::runtime_error(".z: not in packed format");
 
-    if (inbuff[1] != 0x1e)
+    if (g_inbuff[1] != 0x1e)
         throw std::runtime_error(".z: not in packed format");
 
-    inp = &inbuff[2];
-    origsize = 0;
+    char *g_inp = &g_inbuff[2];
+    long g_origsize = 0;
 
-    for (i = 0; i < 4; i++)
-        origsize = origsize * 256 + ((*inp++) & 0377);
+    for (int i = 0; i < 4; ++i)
+        g_origsize = g_origsize * 256 + (*g_inp++ & 0xff);
 
-    maxlev = *inp++ & 0377;
+    short maxlev = *g_inp++ & 0xff;
 
     if (maxlev > 24)
         throw std::runtime_error(".z: not in packed format");
 
-    for (i = 1; i <= maxlev; i++)
-        intnodes[i] = *inp++ & 0377;
+    short g_intnodes[25];
 
-    for (i = 1; i <= maxlev; i++)
+    for (int i = 1; i <= maxlev; ++i)
+        g_intnodes[i] = *g_inp++ & 0377;
+
+    char *g_tree[25];
+    char characters[256];
+    char *g_xeof = &characters[0];
+
+    for (int i = 1; i <= maxlev; ++i)
     {
-        tree[i] = xeof;
+        g_tree[i] = g_xeof;
 
-        for (c = intnodes[i]; c > 0; c--)
+        for (int c = g_intnodes[i]; c > 0; --c)
         {
-            if (xeof >= &characters[255])
+            if (g_xeof >= &characters[255])
                 throw std::runtime_error(".z: not in packed format");
 
-            *xeof++ = *inp++;
+            *g_xeof++ = *g_inp++;
         }
     }
 
-    *xeof++ = *inp++;
-    intnodes[maxlev] += 2;
-    inleft -= inp - &inbuff[0];
+    *g_xeof++ = *g_inp++;
+    g_intnodes[maxlev] += 2;
+    g_inleft -= g_inp - &g_inbuff[0];
 
-    if (inleft < 0)
+    if (g_inleft < 0)
         throw std::runtime_error(".z: not in packed format");
 
-    nchildren = 0;
+    int nchildren = 0;
 
-    for (i = maxlev; i >= 1; i--)
+    for (int i = maxlev; i >= 1; i--)
     {
-        c = intnodes[i];
-        intnodes[i] = nchildren /= 2;
+        int c = g_intnodes[i];
+        g_intnodes[i] = nchildren /= 2;
         nchildren += c;
     }
 
-    return decode();
+    return decode(g_xeof, g_intnodes, g_tree, g_origsize, infile, g_inleft, g_inbuff, g_inp);
 }
 
-int meen(int argc, char **argv)
-{
-    (void)argc;
-    char *pl;
-
-    pl = *argv;
-
-    while (*pl++);  //point pl to end of argv[0] string
-
-    while (--pl >= *argv)
-        if (*pl == '/')
-            break;
-
-    *argv = pl + 1;
-    infile = 0;
-    outfile = 1;
-    return getdict();
-}
-
-int main(int argc, char **argv)
+int main()
 {
     int ret = -1;
 
     try
     {
-        ret = meen(argc, argv);
+        ret = getdict(0);
     }
     catch (const std::exception &e)
     {
