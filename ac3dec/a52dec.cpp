9c5d0fb0c986f123cdc6700a98c0eee5172467ed
Jasper ter Weeme <jterweeme@gmail.com>
2022-10-20 20:58:52 +0200

    a52dec.cpp

--- ac3dec/a52dec.cpp
+++ ac3dec/a52dec.cpp
@@ -21,13 +21,6 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
-#define ATTRIBUTE_ALIGNED_MAX 64
-#define HAVE_DLFCN_H 1
-#define HAVE_FTIME 1
-#define HAVE_GETTIMEOFDAY 1
-#define HAVE_INTTYPES_H 1
-#define HAVE_MEMALIGN 1
-#define HAVE_MEMORY_H 1
 #define HAVE_STDINT_H 1
 #define HAVE_STDLIB_H 1
 #define HAVE_STRINGS_H 1
@@ -35,16 +28,13 @@
 #define HAVE_SYS_STAT_H 1
 #define HAVE_SYS_TIMEB_H 1
 #define HAVE_SYS_TIME_H 1
-#define HAVE_SYS_TYPES_H 1
 #define HAVE_UNISTD_H 1
 #define LIBAO_OSS 
 #define PACKAGE "a52dec"
 #define PACKAGE_BUGREPORT ""
 #define PACKAGE_NAME ""
 #define PACKAGE_STRING ""
-#define PACKAGE_TARNAME ""
 #define PACKAGE_VERSION ""
-#define RETSIGTYPE void
 #define STDC_HEADERS 1
 #define VERSION "0.7.4"
 
@@ -69,22 +59,14 @@ typedef double sample_t;
 
 typedef struct a52_state_s a52_state_t;
 
-#define A52_CHANNEL 0
 #define A52_MONO 1
 #define A52_STEREO 2
-#define A52_3F 3
-#define A52_2F1R 4
-#define A52_3F1R 5
-#define A52_2F2R 6
-#define A52_3F2R 7
-#define A52_CHANNEL1 8
-#define A52_CHANNEL2 9
 #define A52_DOLBY 10
-#define A52_CHANNEL_MASK 15
 
 #define A52_LFE 16
 #define A52_ADJUST_LEVEL 32
 
+extern "C" {
 a52_state_t * a52_init (uint32_t mm_accel);
 sample_t * a52_samples (a52_state_t * state);
 int a52_syncinfo (uint8_t * buf, int * flags,
@@ -95,7 +77,7 @@ void a52_dynrng (a52_state_t * state,
          sample_t (* call) (sample_t, void *), void * data);
 int a52_block (a52_state_t * state);
 void a52_free (a52_state_t * state);
-
+}
 
 typedef struct ao_instance_s ao_instance_t;
 
@@ -112,9 +94,6 @@ typedef struct ao_driver_s {
     char * name;
     ao_open_t * open;
 } ao_driver_t;
-
-/* return NULL terminated array of all drivers */
-ao_driver_t * ao_drivers (void);
 
 static inline ao_instance_t * ao_open (ao_open_t * open)
 {
@@ -141,8 +120,6 @@ static inline void ao_close (ao_instance_t * instance)
 
 
 void float2s16_2 (float * f, int16_t * s16);
-void float2s16_4 (float * f, int16_t * s16);
-void float2s16_5 (float * f, int16_t * s16);
 int channels_multi (int flags);
 void float2s16_multi (float * f, int16_t * s16, int flags);
 void s16_swap (int16_t * s16, int channels);
@@ -196,7 +173,7 @@ typedef struct wav_instance_s {
 static uint8_t wav_header[] = {
     'R', 'I', 'F', 'F', 0xfc, 0xff, 0xff, 0xff, 'W', 'A', 'V', 'E',
     'f', 'm', 't', ' ', 16, 0, 0, 0,
-    1, 0, 2, 0, -1, -1, -1, -1, -1, -1, -1, -1, 4, 0, 16, 0,
+    1, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4, 0, 16, 0,
     'd', 'a', 't', 'a', 0xd8, 0xff, 0xff, 0xff
 };
 
@@ -273,7 +250,7 @@ static ao_instance_t * wav_open (int flags)
 {
     wav_instance_t * instance;
 
-    instance = malloc (sizeof (wav_instance_t));
+    instance = (wav_instance_t *)malloc (sizeof (wav_instance_t));
     if (instance == NULL)
     return NULL;
 
@@ -294,26 +271,6 @@ ao_instance_t * ao_wav_open (void)
     return wav_open (A52_STEREO);
 }
 
-ao_instance_t * ao_wavdolby_open (void)
-{
-    return wav_open (A52_DOLBY);
-}
-  
-
-extern ao_open_t ao_wav_open;
-extern ao_open_t ao_wavdolby_open;
-
-static ao_driver_t audio_out_drivers[] = {
-    {"wav", ao_wav_open},
-    {"wavdolby", ao_wavdolby_open},
-    {NULL, NULL}
-};
-
-ao_driver_t * ao_drivers (void)
-{
-    return audio_out_drivers;
-}
-
 #define BUFFER_SIZE 4096
 static uint8_t buffer[BUFFER_SIZE];
 static FILE * in_file;
@@ -328,35 +285,24 @@ static a52_state_t * state;
 static void handle_args (int argc, char ** argv)
 {
     int c;
-    int i;
-    char * s;
 
-    ao_driver_t *drivers = ao_drivers ();
+    //ao_driver_t *drivers = audio_out_drivers;
     while ((c = getopt (argc, argv, "s::t:crag:o:")) != -1)
         switch (c)
         {
-        case 'o':
-	        for (i = 0; drivers[i].name != NULL; i++)
-                if (strcmp (drivers[i].name, optarg) == 0)
-		            output_open = drivers[i].open;
-
-            if (output_open == NULL)
-                fprintf (stderr, "Invalid video driver: %s\n", optarg);
-	        break;
-	case 'c':
-	    disable_accel = 1;
-	    break;
-	case 'r':
-	    disable_dynrng = 1;
-	    break;
-	case 'a':
-	    disable_adjust = 1;
-	    break;
-	}
+        case 'c':
+            disable_accel = 1;
+            break;
+        case 'r':
+            disable_dynrng = 1;
+            break;
+    	case 'a':
+            disable_adjust = 1;
+            break;
+        }
 
     /* -o not specified, use a default driver */
-    if (output_open == NULL)
-        output_open = drivers[0].open;
+    output_open = ao_wav_open;
 
     if (optind < argc)
     {
@@ -457,7 +403,7 @@ int main (int argc, char ** argv)
 #ifdef HAVE_IO_H
     setmode (fileno (stdout), O_BINARY);
 #endif
-    fprintf (stderr, PACKAGE"-"VERSION
+    fprintf (stderr, "a52dec"
 	     " - by Michel Lespinasse <walken@zoy.org> and Aaron Holtzman\n");
 
     handle_args (argc, argv);
