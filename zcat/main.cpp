0bc9fed9593b8af4ebc95041bceb1ea35f0de8d2
Jasper ter Weeme <jterweeme@gmail.com>
2022-11-12 23:21:33 +0100

    yuvplay

--- zcat/main.cpp
+++ zcat/main.cpp
@@ -11,6 +11,7 @@
 static constexpr uint8_t BLOCK_MODE = 0x80, BITS = 16, INIT_BITS = 9, BIT_MASK = 0x1f;
 static constexpr uint32_t HSIZE = 1<<17;
 static constexpr uint16_t CLEAR = 256;
+static constexpr uint32_t BFSZ = 999999;
 
 class Decomp
 {
@@ -18,21 +19,22 @@ private:
     std::istream *_is;
     std::ostream *_os;
     int _posbits = 0;
-    uint8_t htab[HSIZE];
+    uint8_t _htab[HSIZE];
     uint16_t g_codetab[HSIZE];
     uint32_t _code(uint32_t mask);
     void reset();
-    //std::vector<char> _inbuf;
-    char _inbuf[BUFSIZ + 64];
+    char _inbuf[BFSZ + 64];
     int _insize = 0;
     int _rsize = 0;
     int _inbits = 0;
     int _n_bits = INIT_BITS;
     void dinges();
+    bool _block_mode;
+    int _maxbits;
 public:
     Decomp(std::istream *is, std::ostream *os);
-    void read();
-    int decompress(int maxbits, int block_mode);
+    void init();
+    int decompress();
 };
 
 void Decomp::dinges()
@@ -61,7 +63,7 @@ void Decomp::reset()
 
     if (_insize < 64)
     {
-        std::cin.read(_inbuf + _insize, BUFSIZ);
+        std::cin.read(_inbuf + _insize, BFSZ);
         _rsize = std::cin.gcount();
 
         if (_rsize < 0)
@@ -77,102 +79,110 @@ Decomp::Decomp(std::istream *is, std::ostream *os) : _is(is), _os(os)
 {
 }
 
-void Decomp::read()
+void Decomp::init()
 {
+    if (std::cin.get() != 0x1f)
+        throw "invalid magic";
+
+    if (std::cin.get() != 0x9d)
+        throw "invalid magic";
+
+    uint8_t tmp = std::cin.get();
+    _block_mode = tmp & BLOCK_MODE ? true : false;
+    _maxbits = tmp & BIT_MASK;
+
+    for (uint16_t i = 0; i < 256; ++i)
+        _htab[i] = uint8_t(i);
 }
 
-int Decomp::decompress(int maxbits, int block_mode)
+int Decomp::decompress()
 {
-    _is->read(_inbuf, BUFSIZ);
-    _rsize = _insize = _is->gcount();
     uint32_t bitmask, maxcode;
     maxcode = bitmask = (1<<_n_bits) - 1;
     long oldcode = -1;
     int finchar = 0;
-    uint32_t free_ent = block_mode ? 257 : 256;
+    uint32_t free_ent = _block_mode ? 257 : 256;
 
-    for (uint16_t i = 0; i < 256; ++i)
-        htab[i] = uint8_t(i);
-resetbuf:
-    reset();
-
-    if (oldcode == -1)
+    do
     {
-        uint32_t code = _code(bitmask);
+        reset();
 
-        if (code >= 256)
-            throw "corrupt input";
-
-        _os->put(uint8_t(finchar = int(oldcode = code)));
-    }   
-
-    while (_inbits > _posbits)
-    {
-        if (free_ent > maxcode)
+        if (oldcode == -1)
         {
-            dinges();
-            ++_n_bits;
-            maxcode = _n_bits == maxbits ? 1 << maxbits : (1 << _n_bits) - 1;
-            bitmask = (1 << _n_bits)-1;
-            reset();
-        }
+            uint32_t code = _code(bitmask);
 
-        uint32_t code = _code(bitmask);
-
-        if (code == CLEAR && block_mode)
-        {
-            memset(g_codetab, 0, 256);
-            free_ent = 256;
-            dinges();
-            _n_bits = INIT_BITS;
-            maxcode = (1<<_n_bits) - 1;
-            bitmask = (1<<_n_bits) - 1;
-            reset();
-            continue;
-        }
-
-        long incode = code;
-        uint8_t *stackp = htab + HSIZE - 1;
-
-        if (code >= free_ent)
-        {
-            if (code > free_ent)
+            if (code >= 256)
                 throw "corrupt input";
 
-            *--stackp = uint8_t(finchar);
-            code = oldcode;
+            _os->put(uint8_t(finchar = int(oldcode = code)));
         }
 
-        while (code >= 256)
+        while (_inbits > _posbits)
         {
-            *--stackp = htab[code];
-            code = g_codetab[code];
+            if (free_ent > maxcode)
+            {
+                dinges();
+                ++_n_bits;
+                maxcode = _n_bits == _maxbits ? 1 << _maxbits : (1 << _n_bits) - 1;
+                bitmask = (1 << _n_bits)-1;
+                reset();
+            }
+
+            uint32_t code = _code(bitmask);
+
+            if (code == CLEAR && _block_mode)
+            {
+                memset(g_codetab, 0, 256);
+                free_ent = 256;
+                dinges();
+                _n_bits = INIT_BITS;
+                maxcode = (1<<_n_bits) - 1;
+                bitmask = (1<<_n_bits) - 1;
+                reset();
+                continue;
+            }
+
+            long incode = code;
+            uint8_t *stackp = _htab + HSIZE - 1;
+
+            if (code >= free_ent)
+            {
+                if (code > free_ent)
+                    throw "corrupt input";
+
+                *--stackp = uint8_t(finchar);
+                code = oldcode;
+            }
+
+            while (code >= 256)
+            {
+                *--stackp = _htab[code];
+                code = g_codetab[code];
+            }
+
+            *--stackp = uint8_t(finchar = _htab[code]);
+
+            int i = 0;
+
+            do
+            {
+                _os->write((const char *)(stackp), i);
+                stackp += i;
+                i = (_htab + HSIZE - 1) - stackp;
+            }
+            while (i > 0);
+
+            if ((code = free_ent) < (1 << _maxbits))
+            {
+                g_codetab[code] = uint16_t(oldcode);
+                _htab[code] = uint8_t(finchar);
+                free_ent = code + 1;
+            }
+
+            oldcode = incode;
         }
-
-        *--stackp = uint8_t(finchar = htab[code]);
-
-        int i = 0;
-
-        do
-        {
-            _os->write((const char *)(stackp), i);
-            stackp += i;
-            i = (htab + HSIZE - 1) - stackp;
-        }
-        while (i > 0);
-
-        if ((code = free_ent) < (1 << maxbits))
-        {
-            g_codetab[code] = uint16_t(oldcode);
-            htab[code] = uint8_t(finchar);
-            free_ent = code + 1;
-        }
-
-        oldcode = incode;
     }
-
-    if (_rsize > 0)
-        goto resetbuf;
+    while (_rsize > 0);
 
     return 0;
 }
@@ -186,17 +196,9 @@ int main(int argc, char **argv)
     _setmode(_fileno(stdin), _O_BINARY);
     _setmode(_fileno(stdout), _O_BINARY);
 #endif
-
-    if (std::cin.get() != 0x1f)
-        throw "invalid magic";
-
-    if (std::cin.get() != 0x9d)
-        throw "invalid magic";
-
-    uint8_t tmp = std::cin.get();
     Decomp d(&std::cin, &std::cout);
-    d.read();
-    return d.decompress(tmp & BIT_MASK, tmp & BLOCK_MODE);
+    d.init();
+    return d.decompress();
 }
 
 
