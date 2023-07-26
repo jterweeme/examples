d2123cd0b3eb5eb69ee0a3f6b22eb3c1eadc66c4
Jasper ter Weeme <jterweeme@gmail.com>
2023-07-26 19:37:44 +0200

    netter gemaakt

--- wavefront/obj.cpp
+++ wavefront/obj.cpp
@@ -8,27 +8,22 @@
  * Non triangulated objects wont work!
  * You can use Blender to triangulate
  *
- * g++ obj.c -lglut -lGL -lGLU -lSDL -lSDL_image -fpermissive
+ * g++ obj.cpp -lglut -lGL -lGLU
  *
  */
  
 #include <iostream>
 #include <fstream>
-#include <stdio.h>
-#include <string.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
-#include <iostream>
 #include <sstream>
-#include <fstream>
 #include <string>
 #include <vector>
 #include <cmath>
+#include <array>
  
 #define KEY_ESCAPE 27
- 
-using namespace std;
  
 struct glutWindow
 {
@@ -38,42 +33,30 @@ struct glutWindow
     float z_near;
     float z_far;
 };
- 
- 
- 
-/*************************************************************************** 
-  OBJ Loading 
- ***************************************************************************/
- 
+
 class Model_OBJ
 {
-  public: 
+private:
+    std::vector<float> _normals;
+    std::vector<float> _triangles;
+    long TotalConnectedTriangles;
+    std::array<float, 3> _calcNormal(float *coord1, float *coord2, float *coord3);
+public: 
 	Model_OBJ();			
-    float* calculateNormal(float* coord1,float* coord2,float* coord3 );
     int Load(const char *filename);	// Loads the model
-	void Draw();					// Draws the model on the screen
-	void Release();				// Release the model
- 
-	float* normals;							// Stores the normals
-    float* Faces_Triangles;					// Stores the triangles
-	float* vertexBuffer;					// Stores the points which make the object
-	long TotalConnectedPoints;				// Stores the total number of connected verteces
-	long TotalConnectedTriangles;			// Stores the total number of connected triangles
- 
+	void draw();					// Draws the model on the screen
 };
  
  
 #define POINTS_PER_VERTEX 3
 #define TOTAL_FLOATS_IN_TRIANGLE 9
-using namespace std;
  
 Model_OBJ::Model_OBJ()
 {
     this->TotalConnectedTriangles = 0; 
-    this->TotalConnectedPoints = 0;
 }
- 
-float* Model_OBJ::calculateNormal( float *coord1, float *coord2, float *coord3 )
+
+std::array<float, 3> Model_OBJ::_calcNormal(float *coord1, float *coord2, float *coord3)
 {
     /* calculate Vector1 and Vector2 */
     float va[3], vb[3], vr[3], val;
@@ -93,21 +76,18 @@ float* Model_OBJ::calculateNormal( float *coord1, float *coord2, float *coord3 )
     /* normalization factor */
     val = sqrt( vr[0]*vr[0] + vr[1]*vr[1] + vr[2]*vr[2] );
  
-	//float norm[3];
-    float *norm = new float[3];
+    std::array<float, 3> norm;
 	norm[0] = vr[0]/val;
 	norm[1] = vr[1]/val;
 	norm[2] = vr[2]/val;
  
- 
 	return norm;
 }
- 
- 
+
 int Model_OBJ::Load(const char* filename)
 {
-    string line;
-    ifstream objFile(filename);
+    std::string line;
+    std::ifstream objFile(filename);
 
     if (!objFile.is_open())
     {
@@ -116,29 +96,27 @@ int Model_OBJ::Load(const char* filename)
         return 0;
     }
 
-    objFile.seekg (0, ios::end);				// Go to end of the file, 
-    long fileSize = objFile.tellg();		// get file size
-    objFile.seekg (0, ios::beg);		// we'll use this to register memory for our 3d model
- 
-    vertexBuffer = (float*) malloc (fileSize);		// Allocate memory for the verteces
-    Faces_Triangles = (float*) malloc(fileSize*sizeof(float));	// Allocate memory for the triangles
-    normals  = (float*) malloc(fileSize*sizeof(float));		// Allocate memory for the normals
- 
     int triangle_index = 0;					// Set triangle index to zero
     int normal_index = 0;					// Set normal index to zero
+    long TotalConnectedPoints = 0;
+    std::vector<float> vertexBuf;
  
     while (! objFile.eof() )						// Start reading file data
     {		
         getline (objFile,line);							// Get line from file
  
-        if (line.c_str()[0] == 'v')	// The first character is a v: on this line is a vertex stored.
+        // The first character is a v: on this line is a vertex stored.
+        if (line.c_str()[0] == 'v')	
         {
             line[0] = ' ';		// Set first character to 0. This will allow us to use sscanf
- 
-            sscanf(line.c_str(),"%f %f %f ",		// Read floats from the line: v X Y Z
-					&vertexBuffer[TotalConnectedPoints],
-					&vertexBuffer[TotalConnectedPoints+1], 
-					&vertexBuffer[TotalConnectedPoints+2]);
+            float buf[3];
+
+            // Read floats from the line: v X Y Z
+            sscanf(line.c_str(),"%f %f %f ", &buf[0], &buf[1], &buf[2]);	
+
+            vertexBuf.push_back(buf[0]);
+            vertexBuf.push_back(buf[1]);
+            vertexBuf.push_back(buf[2]);
  
             TotalConnectedPoints += POINTS_PER_VERTEX;	// Add 3 to the total connected points
         }
@@ -169,27 +147,37 @@ int Model_OBJ::Load(const char* filename)
 
 			for (int i = 0; i < POINTS_PER_VERTEX; i++)					
 			{
-					Faces_Triangles[triangle_index + tCounter   ] = vertexBuffer[3*vertexNumber[i] ];
-					Faces_Triangles[triangle_index + tCounter +1 ] = vertexBuffer[3*vertexNumber[i]+1 ];
-					Faces_Triangles[triangle_index + tCounter +2 ] = vertexBuffer[3*vertexNumber[i]+2 ];
-					tCounter += POINTS_PER_VERTEX;
+                _triangles.push_back(vertexBuf[3 * vertexNumber[i] + 0 ]);
+                _triangles.push_back(vertexBuf[3 * vertexNumber[i] + 1 ]);
+                _triangles.push_back(vertexBuf[3 * vertexNumber[i] + 2 ]);
+                tCounter += POINTS_PER_VERTEX;
 			}
  
 			/*********************************************************************
 				 * Calculate all normals, used for lighting
-				 */ 
-			float coord1[3] = { Faces_Triangles[triangle_index], Faces_Triangles[triangle_index+1],Faces_Triangles[triangle_index+2]};
-			float coord2[3] = {Faces_Triangles[triangle_index+3],Faces_Triangles[triangle_index+4],Faces_Triangles[triangle_index+5]};
-			float coord3[3] = {Faces_Triangles[triangle_index+6],Faces_Triangles[triangle_index+7],Faces_Triangles[triangle_index+8]};
-			float *norm = this->calculateNormal( coord1, coord2, coord3 );
+				 */
+            float coord1[3];
+            coord1[0] = _triangles[triangle_index + 0];
+            coord1[1] = _triangles[triangle_index + 1];
+            coord1[2] = _triangles[triangle_index + 2];
+
+            float coord2[3];
+            coord2[0] = _triangles[triangle_index + 3];
+            coord2[1] = _triangles[triangle_index + 4];
+            coord2[2] = _triangles[triangle_index + 5];
+
+            float coord3[3];
+            coord3[0] = _triangles[triangle_index + 6];
+            coord3[1] = _triangles[triangle_index + 7];
+            coord3[2] = _triangles[triangle_index + 8];
+
+            std::array<float, 3> norm = _calcNormal(coord1, coord2, coord3);
  
-			tCounter = 0;
 			for (int i = 0; i < POINTS_PER_VERTEX; i++)
 			{
-				normals[normal_index + tCounter ] = norm[0];
-				normals[normal_index + tCounter +1] = norm[1];
-				normals[normal_index + tCounter +2] = norm[2];
-				tCounter += POINTS_PER_VERTEX;
+                _normals.push_back(norm[0]);
+                _normals.push_back(norm[1]);
+                _normals.push_back(norm[2]);
 			}
 
 			triangle_index += TOTAL_FLOATS_IN_TRIANGLE;
@@ -197,24 +185,17 @@ int Model_OBJ::Load(const char* filename)
 			TotalConnectedTriangles += TOTAL_FLOATS_IN_TRIANGLE;			
 		}	
 	}
-	objFile.close();														// Close OBJ file
+	objFile.close();
+    _triangles.resize(_triangles.size() + 999999);
 	return 0;
 }
  
-void Model_OBJ::Release()
+void Model_OBJ::draw()
 {
-	free(this->Faces_Triangles);
-	free(this->normals);
-	free(this->vertexBuffer);
-}
- 
-void Model_OBJ::Draw()
-{
-	
     glEnableClientState(GL_VERTEX_ARRAY);						// Enable vertex arrays
     glEnableClientState(GL_NORMAL_ARRAY);						// Enable normal arrays
-    glVertexPointer(3,GL_FLOAT,	0,Faces_Triangles);				// Vertex Pointer to triangle array
-    glNormalPointer(GL_FLOAT, 0, normals);						// Normal pointer to normal array
+    glVertexPointer(3,GL_FLOAT,	0, _triangles.data());		    // Vertex Pointer to triangle array
+    glNormalPointer(GL_FLOAT, 0, _normals.data());
     glDrawArrays(GL_TRIANGLES, 0, TotalConnectedTriangles);		// Draw the triangles
     glDisableClientState(GL_VERTEX_ARRAY);						// Disable vertex arrays
     glDisableClientState(GL_NORMAL_ARRAY);						// Disable normal arrays
@@ -248,7 +229,7 @@ void CMain::display()
 	glPushMatrix();
 	glRotatef(45,0,1,0);
 	glRotatef(90,0,1,0);
-    _pthis->obj.Draw();
+    _pthis->obj.draw();
 	glPopMatrix();
 	glutSwapBuffers();
 }
@@ -275,7 +256,7 @@ void CMain::run(int argc, char **argv)
 	win.z_far = 500.0f;
     glutInit(&argc, argv);                                      // GLUT initialization
     glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH );  // Display Mode
-    glutInitWindowSize(win.width,win.height);					// set window size
+    glutInitWindowSize(win.width, win.height);					// set window size
     glutCreateWindow("OpenGL/GLUT OBJ Loader.");
     glutDisplayFunc(display);									// register Display Function
     glutIdleFunc( display );									// register Idle Function
