f4875b447eb518fb550e0c3de894566be9677963
Jasper ter Weeme <jterweeme@gmail.com>
2023-10-07 15:46:31 +0200

    json library

---
+++ json/json.cpp
@@ -0,0 +1,290 @@
+#include "json.h"
+
+void JSONBool::serialize(std::ostream &os)
+{   
+    if (_b)
+        os << "true";
+    else
+        os << "false";
+}
+    
+void JSONProperty::serialize(std::ostream &os)
+{
+    os.put('\"');
+    os << _key;
+    os.put('\"');
+    os.put(':');
+    _value->serialize(os);
+}
+
+void JSONObject::removeProperty(JSONProperty *property)
+{   
+    
+}
+
+bool JSONObject::removeProperty(std::string key)
+{   
+    JSONProperty *prop = nullptr;
+    std::vector<JSONProperty *>::iterator it = begin();
+
+    while (it != end())
+    {
+        JSONProperty *tmp = *it++;
+
+        if (tmp->key().compare(key) == 0)
+            prop = tmp;
+    }       
+
+    if (prop)   
+    {
+        delete prop;
+        _properties.erase(it);
+        return true;
+    }
+
+    //object bevat geen property met gegeven key
+    return false;
+}
+
+JSONObject::~JSONObject()
+{
+    for (auto property : _properties)
+        delete property;
+}
+
+JSONProperty *JSONObject::getProperty(std::string key) const
+{
+    for (auto property : _properties)
+    {
+        if (property->key().compare(key) == 0)
+            return property;
+    }
+
+    return nullptr;
+}
+
+void JSONObject::serialize(std::ostream &os)
+{
+    os.put('{');
+    int len = _properties.size();
+    int i;
+
+    for (i = 0; i < len - 1; ++i)
+    {
+        _properties[i]->serialize(os);
+        os.put(',');
+    }
+
+    if (i < len)
+        _properties[i]->serialize(os);
+    
+    os.put('}');
+}
+    
+JSONArray::~JSONArray()
+{
+    for (auto node : _nodes)
+        delete node;
+}
+
+void JSONArray::serialize(std::ostream &os)
+{
+    os.put('[');
+    int len = _nodes.size();
+    int i;
+
+    for (i = 0; i < len - 1; ++i)
+    {
+        _nodes[i]->serialize(os);
+        os.put(',');
+    }
+
+    if (i < len)
+        _nodes[i]->serialize(os);
+
+    os.put(']');
+}
+
+void JSONArray::append(JSONNode *child)
+{
+    _nodes.push_back(child);
+}
+
+static bool isOneOf(std::string s, char c)
+{
+    return s.find(c) == -1 ? false : true;
+}
+
+void tokenize(std::vector<std::string> &tokens, std::istream &is)
+{           
+    while (true)
+    {       
+        int c = is.get();
+
+        if (c == EOF)
+            break;
+
+        if (isOneOf("[]{}:,", c))
+        {
+            tokens.push_back(std::string(1, c));
+            continue;
+        }
+                
+        if (isOneOf("0123456789-.", c))
+        {
+            std::string token;
+            token.push_back(c);
+
+            while (true)
+            {
+                c = is.peek();
+
+                if (isOneOf("0123456789-.", c) == false)
+                    break;
+
+                token.push_back(c);
+                is.get();
+            }
+    
+            tokens.push_back(token);
+            continue;
+        }
+
+        if (isalpha(c))
+        {
+            std::string token;
+
+            while (true)
+            {
+                token.push_back(c);
+                c = is.peek(); 
+
+                if (isalpha(c) == false)
+                    break;
+
+                c = is.get();
+            }
+
+            tokens.push_back(token);
+            continue;
+        }
+
+        if (c == '\"')
+        {
+            std::string token;
+            token.push_back(c);
+
+            do
+            {
+                c = is.get();
+                token.push_back(c);
+            }
+            while (c != '\"');
+
+            tokens.push_back(token);
+            continue;
+        }
+    }
+}
+
+typedef std::vector<std::string>::const_iterator cvecstrit;
+
+void parse(cvecstrit it, cvecstrit end, JSONNode *parent)
+{
+    std::vector<JSONNode *> stack;
+    stack.push_back(parent);
+    std::string peek;
+
+    while (it != end)
+    {
+        JSONProperty *prop = dynamic_cast<JSONProperty *>(stack.back());
+        std::string token = *it++;
+        peek.clear();
+
+        if (it != end)
+            peek = *it;
+
+        if (token.compare(",") == 0)
+        {
+            if (prop)
+                stack.pop_back();
+
+            continue;
+        }
+
+        if (peek.compare(":") == 0)
+        {
+            std::string s = token.substr(1, token.length() - 2);
+            JSONProperty *p = new JSONProperty(s);
+            JSONObject *o = dynamic_cast<JSONObject *>(stack.back());
+            o->appendProperty(p);
+            stack.push_back(p);
+            ++it;
+            continue;
+        }
+
+        if (token.compare("null") == 0)
+        {
+            stack.back()->append(new JSONNull());
+            continue;
+        }
+
+        if (token.compare("true") == 0)
+        {
+            stack.back()->append(new JSONBool(true));
+            continue;
+        }
+
+        if (token.compare("false") == 0)
+        {
+            stack.back()->append(new JSONBool(false));
+            continue;
+        }
+
+        if (token.compare("[") == 0)
+        {
+            JSONArray *a = new JSONArray();
+            stack.back()->append(a);
+            stack.push_back(a);
+            continue;
+        }
+
+        if (token.compare("]") == 0)
+        {
+            stack.pop_back();
+            continue;
+        }
+
+        if (token.compare("{") == 0)
+        {
+            JSONObject *o = new JSONObject();
+            stack.back()->append(o);
+            stack.push_back(o);
+            continue;
+        }
+
+        if (token.compare("}") == 0)
+        {
+            stack.pop_back();
+            stack.pop_back();
+            continue;
+        }
+
+        if (token[0] == '\"')
+        {
+            std::string s = token.substr(1, token.length() - 2);
+            stack.back()->append(new JSONString(s));
+            continue;
+        }
+
+        if (isdigit(token[0]) || token[0] == '-')
+        {
+            stack.back()->append(new JSONNumber(token));
+            continue;
+        }
+
+        std::cerr << token << "\r\n";
+        throw "Onbekend token!";
+    }
+}
+
---
+++ json/json.h
@@ -0,0 +1,114 @@
+#ifndef JSON_H
+#define JSON_H
+#include <iostream>
+#include <vector>
+
+class JSONNode
+{
+public:
+    virtual ~JSONNode() { }
+    virtual void append(JSONNode *n) { std::cerr << "Append error\r\n"; throw "Error"; }
+    virtual void serialize(std::ostream &os) { }
+};
+
+class JSONRoot : public JSONNode
+{
+private:
+    JSONNode *_root;
+public:
+    ~JSONRoot() { delete _root; }
+    JSONNode *root() const { return _root; }
+    void append(JSONNode *n) override { _root = n; }
+    void serialize(std::ostream &os) override { _root->serialize(os); }
+};
+
+class JSONString : public JSONNode
+{
+private:
+    std::string _s;
+public:
+    JSONString() { }
+    JSONString(std::string s) : _s(s) { }
+    std::string s() const { return _s; }
+    void serialize(std::ostream &os) override { os << "\"" << _s << "\""; }
+};
+
+class JSONBool : public JSONNode
+{
+private:
+    bool _b;
+public:
+    JSONBool() { }
+    JSONBool(bool b) : _b(b) { }
+    void serialize(std::ostream &os) override;
+};
+
+class JSONNumber : public JSONNode
+{
+private:
+    std::string _n;
+public:
+    JSONNumber() { }
+    JSONNumber(int n) { _n = std::to_string(n); }
+    JSONNumber(std::string n) : _n(n) { }
+    void serialize(std::ostream &os) override { os << _n; }
+};
+
+class JSONProperty : public JSONNode
+{
+private:
+    std::string _key;
+    JSONNode *_value;
+public:
+    JSONProperty() { }
+    JSONProperty(std::string key) : _key(key) { }
+    JSONProperty(std::string key, JSONNode *val) : _key(key), _value(val) { }
+    ~JSONProperty() { delete _value; }
+    std::string key() const { return _key; }
+    void append(JSONNode *n) override { _value = n; }
+    void serialize(std::ostream &os) override;
+};
+
+class JSONObject : public JSONNode
+{
+private:
+    std::vector<JSONProperty *> _properties;
+public:
+    JSONObject() { }
+    ~JSONObject();
+    void appendProperty(JSONProperty *p) { _properties.push_back(p); }
+    void append(std::string key, JSONNode *n) { appendProperty(new JSONProperty(key, n)); }
+    void append(std::string key, std::string value) { append(key, new JSONString(value)); }
+    std::vector<JSONProperty *>::iterator begin() { return _properties.begin(); }
+    std::vector<JSONProperty *>::iterator end() { return _properties.end(); }
+    JSONProperty *getProperty(std::string key) const;
+    void removeProperty(JSONProperty *property);
+    bool removeProperty(std::string key);
+    void serialize(std::ostream &os) override;
+};
+
+class JSONArray : public JSONNode
+{
+private:
+    std::vector<JSONNode *> _nodes;
+public:
+    ~JSONArray();
+    void append(JSONNode *child);
+    void serialize(std::ostream &os);
+};
+
+class JSONNull : public JSONNode
+{
+public:
+    void serialize(std::ostream &os) { os << "null"; }
+};
+
+typedef std::vector<std::string>::const_iterator cvecstrit;
+
+void tokenize(std::vector<std::string> &tokens, std::istream &is);
+void parse(cvecstrit it, cvecstrit end, JSONNode *parent);
+
+
+#endif
+
+
