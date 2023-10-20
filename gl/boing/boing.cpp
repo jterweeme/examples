98945e55efd0a6bbddead8b2295e0c4f874f7809
Jasper ter Weeme <jterweeme@gmail.com>
2023-10-20 18:14:22 +0200

    niet gebruikte linmath functies weggehaald

--- gl/boing/boing.cpp
+++ gl/boing/boing.cpp
@@ -31,7 +31,124 @@
 #include <stdlib.h>
 #include <math.h>
 #include <GLFW/glfw3.h>
-#include "linmath.h"
+
+typedef float vec3[3];
+typedef float vec4[4];
+typedef vec4 mat4x4[4];
+
+static void vec3_sub(vec3 r, vec3 const a, vec3 const b)
+{
+    for (int i = 0; i < 3; ++i)
+        r[i] = a[i] - b[i];
+}
+
+static void vec3_scale(vec3 r, vec3 const v, float const s)
+{
+    for (int i = 0; i < 3; ++i)
+        r[i] = v[i] * s;
+}
+
+static float vec3_mul_inner(vec3 const a, vec3 const b)
+{
+    float p = 0.f;
+    for (int i = 0; i < 3; ++i)
+        p += b[i] * a[i];
+    return p;
+}
+
+static float vec4_mul_inner(vec4 const a, vec4 const b)
+{
+    float p = 0.f;
+    for (int i = 0; i < 4; ++i)
+        p += b[i] * a[i];
+    return p;
+}
+
+static float vec3_len(vec3 const v)
+{
+    return sqrtf(vec3_mul_inner(v, v));
+}
+
+static void vec3_norm(vec3 r, vec3 const v)
+{
+    float k = 1.f / vec3_len(v);
+    vec3_scale(r, v, k);
+}
+
+static void vec3_mul_cross(vec3 r, vec3 const a, vec3 const b)
+{
+    r[0] = a[1]*b[2] - a[2]*b[1];
+    r[1] = a[2]*b[0] - a[0]*b[2];
+    r[2] = a[0]*b[1] - a[1]*b[0];
+}
+
+static void mat4x4_row(vec4 r, mat4x4 const M, int i)
+{
+    for (int k=0; k<4; ++k)
+        r[k] = M[k][i];
+}
+
+static void mat4x4_translate_in_place(mat4x4 M, float x, float y, float z)
+{
+    vec4 t = {x, y, z, 0};
+    vec4 r;
+    for (int i = 0; i < 4; ++i) {
+        mat4x4_row(r, M, i);
+        M[3][i] += vec4_mul_inner(r, t);
+    }
+}
+
+static void mat4x4_perspective(mat4x4 m, float y_fov, float aspect, float n, float f)
+{
+    /* NOTE: Degrees are an unhandy unit to work with.
+     * linmath.h uses radians for everything! */
+    float const a = 1.f / tanf(y_fov / 2.f);
+
+    m[0][0] = a / aspect;
+    m[0][1] = 0.f;
+    m[0][2] = 0.f;
+    m[0][3] = 0.f;
+
+    m[1][0] = 0.f;
+    m[1][1] = a;
+    m[1][2] = 0.f;
+    m[1][3] = 0.f;
+
+    m[2][0] = 0.f;
+    m[2][1] = 0.f;
+    m[2][2] = -((f + n) / (f - n));
+    m[2][3] = -1.f;
+
+    m[3][0] = 0.f;
+    m[3][1] = 0.f;
+    m[3][2] = -((2.f * f * n) / (f - n));
+    m[3][3] = 0.f;
+}
+
+static void mat4x4_look_at(mat4x4 m, vec3 const eye, vec3 const center, vec3 const up)
+{
+    vec3 f, s, t;
+    vec3_sub(f, center, eye);   
+    vec3_norm(f, f);
+    vec3_mul_cross(s, f, up);
+    vec3_norm(s, s);
+    vec3_mul_cross(t, s, f);
+
+    for (int i = 0; i <= 2; ++i)
+    {
+        m[i][0] =  s[i];
+        m[i][1] =  t[i];
+        m[i][2] = -f[i];
+        m[i][3] =   0.f;
+    }
+
+    m[3][0] =  0.f;
+    m[3][1] =  0.f;
+    m[3][2] =  0.f;
+    m[3][3] =  1.f;
+
+    mat4x4_translate_in_place(m, -eye[0], -eye[1], -eye[2]);
+}
 
 #define RADIUS           70.f
 #define STEP_LONGITUDE   22.5f                   /* 22.5 makes 8 bands like original Boing */
@@ -525,40 +642,39 @@ static void display()
 
 int main()
 {
-   GLFWwindow* window;
+    GLFWwindow* window;
 
-   /* Init GLFW */
-   if( !glfwInit() )
-      exit( EXIT_FAILURE );
+    /* Init GLFW */
+    if( !glfwInit() )
+        exit( EXIT_FAILURE );
 
-   window = glfwCreateWindow( 400, 400, "Boing (classic Amiga demo)", NULL, NULL );
-   if (!window)
-   {
-       glfwTerminate();
-       exit( EXIT_FAILURE );
-   }
+    window = glfwCreateWindow( 400, 400, "Boing (classic Amiga demo)", NULL, NULL );
+    if (!window)
+    {
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
 
-   glfwSetWindowAspectRatio(window, 1, 1);
+    glfwSetWindowAspectRatio(window, 1, 1);
 
-   glfwSetFramebufferSizeCallback(window, reshape);
-   glfwSetKeyCallback(window, key_callback);
-   glfwSetMouseButtonCallback(window, mouse_button_callback);
-   glfwSetCursorPosCallback(window, cursor_position_callback);
+    glfwSetFramebufferSizeCallback(window, reshape);
+    glfwSetKeyCallback(window, key_callback);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetCursorPosCallback(window, cursor_position_callback);
 
-   glfwMakeContextCurrent(window);
-   //gladLoadGL(glfwGetProcAddress);
-   glfwSwapInterval( 1 );
+    glfwMakeContextCurrent(window);
+    //gladLoadGL(glfwGetProcAddress);
+    glfwSwapInterval( 1 );
 
-   glfwGetFramebufferSize(window, &width, &height);
-   reshape(window, width, height);
+    glfwGetFramebufferSize(window, &width, &height);
+    reshape(window, width, height);
 
-   glfwSetTime( 0.0 );
+    glfwSetTime( 0.0 );
 
-   init();
+    init();
 
-   /* Main loop */
-   for (;;)
-   {
+    for (;;)
+    {
        /* Timing */
        t = glfwGetTime();
        dt = t - t_old;
@@ -574,9 +690,10 @@ int main()
        /* Check if we are still running */
        if (glfwWindowShouldClose(window))
            break;
-   }
+    }
 
-   glfwTerminate();
-   exit( EXIT_SUCCESS );
+    glfwTerminate();
+    exit( EXIT_SUCCESS );
+    return 0;
 }
 
