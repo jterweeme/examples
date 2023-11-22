94587e58ac127485892e1bb4cbe497b39ce2180a
Jasper ter Weeme <jterweeme@gmail.com>
2023-11-23 00:53:59 +0100

    weg met goto zcat.cpp

--- zcat/zcat.cpp
+++ zcat/zcat.cpp
@@ -60,30 +60,35 @@ uint32_t BitInputStream::readUint(uint32_t numBits)
     return result;
 }
 
-int main()
+uint8_t inbuf[BUFSIZ + 64];
+int rsize;
+int insize;
+uint32_t bitmask;
+uint32_t posbits;
+uint32_t inbits;
+uint8_t *p;
+uint8_t n_bits;
+
+uint32_t getCode(uint8_t n_bits)
 {
-    BitInputStream bis(std::cin);
-    uint8_t inbuf[BUFSIZ + 64];  //Input buffer
-    int rsize = read(0, inbuf, BUFSIZ);
-    int insize = rsize;
-    assert(insize >= 3);
-    assert(inbuf[0] == 0x1f);
-    assert(inbuf[1] == 0x9d);
-    const uint8_t maxbits = inbuf[2] & 0x1f;
-    const uint8_t block_mode = inbuf[2] & 0x80;
-    assert(maxbits <= 16);
-    const uint32_t maxmaxcode = 1 << maxbits;
-    uint8_t n_bits = 9;
-    uint32_t bitmask = (1 << n_bits) - 1;
-    uint32_t maxcode = n_bits == maxbits ? maxmaxcode : (1 << n_bits) - 1;
-    int32_t oldcode = -1;
-    uint8_t finchar = 0;
-    uint32_t posbits = 3<<3;
-    uint32_t free_ent = block_mode ? 257 : 256;
-    uint16_t codetab[HSIZE] = {0};
-    uint8_t htab[HSIZE];
-    std::iota(htab, htab + 256, 0);
-resetbuf:
+    p = inbuf + (posbits >> 3);
+
+    uint32_t code = ((uint32_t(p[0]) | uint32_t(p[1]) << 8 | uint32_t(p[2]) << 16)
+                            >> (posbits & 0x7)) & bitmask;
+
+    posbits += n_bits;
+    return code;
+}
+
+void clear()
+{
+    posbits = (posbits - 1) + ((n_bits<<3) - (posbits - 1 + (n_bits<<3)) % (n_bits<<3));
+    n_bits = 9;
+    bitmask = (1 << n_bits) - 1;
+}
+
+void bufread()
+{
     int o = posbits >> 3;
     int e = o <= insize ? insize - o : 0;
 
@@ -100,7 +105,31 @@ resetbuf:
         insize += rsize;
     }
 
-    uint32_t inbits = rsize > 0 ? insize - insize % n_bits << 3 : (insize << 3) - (n_bits - 1);
+    inbits = rsize > 0 ? insize - insize % n_bits << 3 : (insize << 3) - (n_bits - 1);
+}
+
+int main()
+{
+    rsize = read(0, inbuf, BUFSIZ);
+    insize = rsize;
+    assert(insize >= 3);
+    assert(inbuf[0] == 0x1f);
+    assert(inbuf[1] == 0x9d);
+    const uint8_t maxbits = inbuf[2] & 0x1f;
+    const uint8_t block_mode = inbuf[2] & 0x80;
+    assert(maxbits <= 16);
+    const uint32_t maxmaxcode = 1 << maxbits;
+    n_bits = 9;
+    bitmask = (1 << n_bits) - 1;
+    uint32_t maxcode = n_bits == maxbits ? maxmaxcode : (1 << n_bits) - 1;
+    int32_t oldcode = -1;
+    uint8_t finchar = 0;
+    posbits = 3<<3;
+    uint32_t free_ent = block_mode ? 257 : 256;
+    uint16_t codetab[HSIZE] = {0};
+    uint8_t htab[HSIZE];
+    std::iota(htab, htab + 256, 0);
+    bufread();
 
     while (inbits > posbits)
     {
@@ -110,15 +139,11 @@ resetbuf:
             ++n_bits;
             maxcode = n_bits == maxbits ? maxmaxcode : (1<<n_bits) - 1;
             bitmask = (1 << n_bits) - 1;
-            goto resetbuf;
+            bufread();
+            continue;
         }
 
-        char_type *p = inbuf + (posbits >> 3);
-
-        uint32_t code = ((uint32_t(p[0]) | uint32_t(p[1]) << 8 | uint32_t(p[2]) << 16)
-                            >> (posbits & 0x7)) & bitmask;
-
-        posbits += n_bits;
+        uint32_t code = getCode(n_bits);
 
         if (oldcode == -1)
         {
@@ -133,16 +158,14 @@ resetbuf:
         {
             memset(codetab, 0, 256);
             free_ent = 256;
-            posbits = (posbits - 1) + ((n_bits<<3) - (posbits - 1 + (n_bits<<3)) % (n_bits<<3));
-            n_bits = 9;
-            bitmask = (1 << n_bits) - 1;
+            clear();
             maxcode = n_bits == maxbits ? maxmaxcode : (1 << n_bits) - 1;
-            goto resetbuf;
+            bufread();
+            continue;
         }
 
         code_int incode = code;
-        //char_type *stackp = htab + HSIZE * 4 - 4;
-        char_type *stackp = htab + HSIZE - 1;
+        uint8_t *stackp = htab + HSIZE - 1;
 
         //Special case for KwKwK string.
         if (code >= free_ent)   
@@ -175,10 +198,13 @@ resetbuf:
 
         //remember previous code
         oldcode = incode;
-    }
 
-    if (rsize > 0)
-        goto resetbuf;
+        if (inbits <= posbits && rsize > 0)
+        {
+            bufread();
+            continue;
+        }
+    }
 
     std::cout.flush();
     return 0;
