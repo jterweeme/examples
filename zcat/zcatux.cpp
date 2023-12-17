1221329bd6e2cd374e7559f950b7e89cfb657e17
Jasper ter Weeme <jterweeme@gmail.com>
2023-12-17 14:00:51 +0100

    zcatux met unistd read en write

--- zcat/zcatux.cpp
+++ zcat/zcatux.cpp
@@ -3,17 +3,54 @@
 
 #include <cassert>
 #include <cstdint>
-#include <iostream>
-#include <fstream>
+#include <cstdio>
 #include <vector>
+#include <unistd.h>
+#include <fcntl.h>
+
+class InStream
+{
+    int _fd;
+    uint32_t _cap;
+    uint32_t _head = 0, _tail = 0;
+    uint8_t *_buf;
+public:
+    InStream(int fd, uint32_t capacity) : _fd(fd), _cap(capacity), _buf(new uint8_t[capacity]) { }
+    ~InStream() { delete[] _buf; }
+
+    int get()
+    {
+        if (_tail == _head)
+        {
+            ssize_t n = ::read(_fd, _buf, _cap);
+            if (n < 1) return -1;
+            _head = n;
+            _tail = 0;
+        }
+        return _buf[_tail++];
+    }
+};
+
+class OutStream
+{
+    int _fd;
+    uint32_t _cap;
+    uint32_t _pos = 0;
+    char *_buf;
+public:
+    OutStream(int fd, uint32_t capacity) : _fd(fd), _cap(capacity), _buf(new char[capacity]) { }
+    ~OutStream() { delete[] _buf; }
+    void put(char c) { if (_pos > _cap) flush(); _buf[_pos++] = c; }
+    void flush() { ::write(_fd, _buf, _pos), _pos = 0; }
+};
 
 class BitStream
 {
-    std::istream &_is;
+    InStream &_is;
     unsigned _bits = 0, _window = 0;
 public:
     unsigned cnt = 0;
-    BitStream(std::istream &is) : _is(is) { }
+    BitStream(InStream &is) : _is(is) { }
 
     int readBits(unsigned n)
     {
@@ -36,12 +73,12 @@ class LZW
     unsigned _oldcode, _n_bits = 9;
     char _finchar;
     std::vector<std::pair<unsigned, char>> _dict;
-    std::ostream &_os;
+    OutStream &_os;
 public:
     unsigned n_bits() const { return _n_bits; }
     void reset() { _n_bits = 9; _dict.clear(); }
 
-    LZW(unsigned maxbits, bool block_mode, char first, std::ostream &os)
+    LZW(unsigned maxbits, bool block_mode, char first, OutStream &os)
       : _maxbits(maxbits), _oldcode(first), _finchar(first), _os(os)
     {
         //why did they design it this way?
@@ -78,14 +115,19 @@ public:
 
 int main(int argc, char **argv)
 {
-    auto const os = &std::cout;
-    auto is = &std::cin;
-    std::ifstream ifs;
-
+    OutStream stout(1, BUFSIZ);
+    auto const os = &stout;
+    
+    int fd = 0; //stdin
+    
     if (argc > 1)
-        ifs.open(argv[1]), is = &ifs;
+    {
+        fd = open(argv[1], O_RDONLY);
+        assert(fd != -1);
+    }
 
-    BitStream bis(*is);
+    InStream is(fd, BUFSIZ);
+    BitStream bis(is);
     assert(bis.readBits(16) == 0x9d1f);
     const unsigned maxbits = bis.readBits(7);
     assert(maxbits <= 16);
@@ -117,7 +159,7 @@ int main(int argc, char **argv)
     }
 
     os->flush();
-    ifs.close();
+    //ifs.close();
     return 0;
 }
 
