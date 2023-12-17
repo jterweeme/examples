a99ad37b00425aec78d8ec4527b1de3570eee788
Jasper ter Weeme <jterweeme@gmail.com>
2023-12-17 14:34:10 +0100

    zcatux samengevoegd met zcatpp

--- zcat/zcatpp.cpp
+++ zcat/zcatpp.cpp
@@ -1,19 +1,67 @@
 //This is a comment
 //I love comments
 
+#define SNEL 1
+
 #include <cassert>
 #include <cstdint>
+#include <vector>
+#ifdef SNEL
+#include <unistd.h>
+#include <fcntl.h>
+#else
 #include <iostream>
 #include <fstream>
-#include <vector>
+#endif
+
+#ifdef SNEL
+class InStream
+{
+    int _fd;
+    uint32_t _cap;
+    uint32_t _head = 0, _tail = 0;
+    uint8_t *_buf;
+public:
+    InStream(int fd, uint32_t capacity) : _fd(fd), _cap(capacity), _buf(new uint8_t[capacity]) { }
+    ~InStream() { delete[] _buf; }
+
+    int get()
+    {
+        if (_tail == _head)
+        {
+            ssize_t n = ::read(_fd, _buf, _cap);
+            if (n < 1) return -1;
+            _head = n;
+            _tail = 0;
+        }
+        return _buf[_tail++];
+    }
+};
+
+class OutStream
+{
+    int _fd;
+    uint32_t _cap;
+    uint32_t _pos = 0;
+    char *_buf;
+public:
+    OutStream(int fd, uint32_t capacity) : _fd(fd), _cap(capacity), _buf(new char[capacity]) { }
+    ~OutStream() { delete[] _buf; }
+    void put(char c) { if (_pos > _cap) flush(); _buf[_pos++] = c; }
+    void flush() { ::write(_fd, _buf, _pos), _pos = 0; }
+};
+#else
+typedef std::istream InStream;
+typedef std::ostream OutStream;
+#endif
 
 class BitStream
 {
-    std::istream &_is;
+    InStream &_is;
     unsigned _bits = 0, _window = 0;
 public:
     unsigned cnt = 0;
-    BitStream(std::istream &is) : _is(is) { }
+    BitStream(InStream &is) : _is(is) { }
 
     int readBits(unsigned n)
     {
@@ -36,12 +84,12 @@ class LZW
     unsigned _oldcode, _n_bits = 9;
     char _finchar;
     std::vector<std::pair<unsigned, char>> _dict;
-    std::ostream &_os;
+    OutStream &_os;
 public:
     unsigned n_bits() const { return _n_bits; }
     void reset() { _n_bits = 9; _dict.clear(); }
 
-    LZW(unsigned maxbits, bool block_mode, char first, std::ostream &os)
+    LZW(unsigned maxbits, bool block_mode, char first, OutStream &os)
       : _maxbits(maxbits), _oldcode(first), _finchar(first), _os(os)
     {
         //why did they design it this way?
@@ -78,13 +126,21 @@ public:
 
 int main(int argc, char **argv)
 {
+#if SNEL
+    OutStream ous(1, 8192);
+    auto const os = &ous;
+    int fd = argc > 1 ? ::open(argv[1], O_RDONLY) : 0; //stdin
+    assert(fd != -1);
+    InStream ins(fd, 8192);
+    InStream *is = &ins;
+#else
     auto const os = &std::cout;
     auto is = &std::cin;
     std::ifstream ifs;
 
     if (argc > 1)
         ifs.open(argv[1]), is = &ifs;
-
+#endif
     BitStream bis(*is);
     assert(bis.readBits(16) == 0x9d1f);
     const unsigned maxbits = bis.readBits(7);
@@ -117,7 +173,11 @@ int main(int argc, char **argv)
     }
 
     os->flush();
+#ifdef SNEL
+    ::close(fd);
+#else
     ifs.close();
+#endif
     return 0;
 }
 
