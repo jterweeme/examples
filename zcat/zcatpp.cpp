54aebf0a6fafc8880762cb1850ec14a098904986
Jasper ter Weeme <jterweeme@gmail.com>
2023-12-22 13:25:17 +0100

    versimpeling

--- zcat/zcatpp.cpp
+++ zcat/zcatpp.cpp
@@ -19,17 +19,17 @@
 #ifdef FAST
 class istream
 {
-    int _fd;
+private:
     uint32_t _cap;
     uint32_t _head = 0, _tail = 0;
     uint8_t *_buf;
+protected:
+    int _fd;
 public:
     ~istream() { delete[] _buf; }
 
-    istream(int fd, uint32_t capacity)
-      :
-        _fd(fd), _cap(capacity), _buf(new uint8_t[capacity])
-    { }
+    istream(int fd = -1, uint32_t capacity = 8192)
+      : _cap(capacity), _buf(new uint8_t[capacity]), _fd(fd) { }
 
     int get()
     {
@@ -42,6 +42,13 @@ public:
         }
         return _buf[_tail++];
     }
+};
+
+class ifstream : public istream
+{
+public:
+    void close() { ::close(_fd); }
+    void open(const char *fn) { _fd = ::open(fn, O_RDONLY); }
 };
 
 class ostream
@@ -57,10 +64,12 @@ public:
     void flush() { ::write(_fd, _buf, _pos), _pos = 0; }
 };
 
+static istream cin(0, 8192);
 static ostream cout(1, 8192);
 #else
 using std::istream;
 using std::ostream;
+using std::ifstream;
 using std::cin;
 using std::cout;
 #endif
@@ -88,6 +97,14 @@ public:
     }
 };
 
+class PrintStack
+{
+    std::vector<char> _stack;
+public:
+    void push_back(char c) { _stack.push_back(c); }
+    void print(ostream &os) { for (; _stack.size(); _stack.pop_back()) os.put(_stack.back()); }
+};
+
 class LZW
 {
     const unsigned _maxbits;
@@ -95,33 +112,26 @@ class LZW
     char _finchar;
     std::vector<std::pair<unsigned, char>> _dict;
     ostream &_os;
+    PrintStack _stack;
 public:
     void clear_dict() { _dict.clear(); }
 
     LZW(unsigned maxbits, bool block_mode, char first, ostream &os)
-      : _maxbits(maxbits), _oldcode(first), _finchar(first), _os(os)
-    {
-        //why did they design it this way?
-        if (block_mode)
-            _dict.push_back(std::pair<unsigned, char>(0, 0));
-    }
+      : _maxbits(maxbits), _oldcode(first), _finchar(first), _os(os) { }
 
     inline void code(const unsigned in)
     {
         assert(in >= 0 && in <= 65535 && in <= _dict.size() + 256);
         auto c = in;
-        std::vector<char> stack;
 
         if (c == _dict.size() + 256)
-            stack.push_back(_finchar), c = _oldcode;
+            _stack.push_back(_finchar), c = _oldcode;
 
         for (; c >= 256U; c = _dict[c - 256].first)
-            stack.push_back(_dict[c - 256].second);
+            _stack.push_back(_dict[c - 256].second);
 
         _os.put(_finchar = c);
-
-        for (; stack.size(); stack.pop_back())
-            _os.put(stack.back());
+        _stack.print(_os);
 
         if (_dict.size() + 256 < 1U << _maxbits)
             _dict.push_back(std::pair<unsigned, char>(_oldcode, _finchar));
@@ -132,19 +142,13 @@ public:
 
 int main(int argc, char **argv)
 {
-    auto const os = &cout;
-#if FAST
-    int fd = argc > 1 ? ::open(argv[1], O_RDONLY) : 0; //stdin
-    assert(fd != -1);
-    istream ins(fd, 8192);
-    auto is = &ins;
-#else
-    auto is = &cin;
-    std::ifstream ifs;
+    istream *is = &cin;
+    ostream *os = &cout;
+    ifstream ifs;
 
     if (argc > 1)
         ifs.open(argv[1]), is = &ifs;
-#endif
+
     BitStream bis(*is);
     assert(bis.readBits(16) == 0x9d1f);
     const unsigned maxbits = bis.readBits(7);
@@ -155,8 +159,8 @@ int main(int argc, char **argv)
     unsigned cnt = 1, nbits = 9;
     int first = bis.readBits(nbits);
     assert(first >= 0 && first < 256);
-    os->put(first);
     LZW lzw(maxbits, block_mode, first, *os);
+    lzw.code(first);
 
     for (int code; (code = bis.readBits(nbits)) != -1;)
     {
@@ -182,11 +186,7 @@ int main(int argc, char **argv)
     }
 
     os->flush();
-#ifdef FAST
-    ::close(fd);
-#else
     ifs.close();
-#endif
     return 0;
 }
 
